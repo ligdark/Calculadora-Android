ead9491d11b966f28c741fc910ea699938e2b5e7
ligdark <wln16@bol.com.br>
2015-05-06 17:49:01 -0300

    a

--- app/src/main/java/com/example/williancordeiro/calculadora2/MainActivity.java
+++ app/src/main/java/com/example/williancordeiro/calculadora2/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +14,19 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Button b = new Button(this);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(v.getContext(), "Toast é mto simples", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        });
         setContentView(R.layout.activity_main);
+
+
     }
 
 
